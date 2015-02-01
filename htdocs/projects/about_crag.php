876f7cf439b70ea48df0e2a1a7299bbb45bf5360
John McFarlane <mcfarlane.john@gmail.com>
2015-02-01 12:43:36 -0800

    updates Crag project info
    - removes broken download link
    - cleans up text
    - adds more recent image

--- htdocs/projects/about_crag.php
+++ htdocs/projects/about_crag.php
@@ -3,7 +3,6 @@
 <p>This is a long-term project to simulate planetary bodies down to fine detail in real-time.
 It's also a place for me to experiment with AI, C++, OpenGL and development skills in general.</p>
 
-<img src="http://crag.googlecode.com/files/crag_screen_3.jpg" alt="screen shot from the crag project showing a planet partially obscured by a moon" />
+<img src="crag.png" alt="screen shot from the crag project showing a planet partially obscured by a moon" width="640" height="360" />
 
-<p>Crag is hosted on <a href="http://code.google.com/p/crag/">Google Code</a>. You can find Android, Linux, OS X and Windows versions of the simulation on the <a href="http://code.google.com/p/crag/wiki/Downloads">downloads</a> page.</p>
-
+<p>Crag is hosted on <a href="http://code.google.com/p/crag/">Google Code</a> and includes downloads for Android and Windows as well as instructions for building on Linux and Raspberry Pi.</p>
