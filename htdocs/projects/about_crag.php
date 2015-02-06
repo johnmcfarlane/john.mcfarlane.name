a6524014841f576592c7c9d1651d3993ce89facc
John McFarlane <mcfarlane.john@gmail.com>
2015-02-05 21:34:26 -0800

    redirects links to Crag project from Google Code to GitHub
    - correct alt attribute of recently changed img element

--- htdocs/projects/about_crag.php
+++ htdocs/projects/about_crag.php
@@ -3,6 +3,6 @@
 <p>This is a long-term project to simulate planetary bodies down to fine detail in real-time.
 It's also a place for me to experiment with AI, C++, OpenGL and development skills in general.</p>
 
-<img src="crag.png" alt="screen shot from the crag project showing a planet partially obscured by a moon" width="640" height="360" />
+<img src="crag.png" alt="screen shot from the crag project" width="512" height="288" />
 
-<p>Crag is hosted on <a href="http://code.google.com/p/crag/">Google Code</a> and includes downloads for Android and Windows as well as instructions for building on Linux and Raspberry Pi.</p>
+<p>Crag is hosted on <a href="https://github.com/johnmcfarlane/crag">GitHub</a> and includes downloads for Android and Windows as well as instructions for building on Linux and Raspberry Pi.</p>
